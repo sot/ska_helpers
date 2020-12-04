e76939faaa0e5f6899a3b4bc4a4efb6ecf0fa526
Tom Aldcroft <taldcroft@gmail.com>
2020-12-04 11:07:25 -0500

    Add license

--- ska_helpers/retry/__init__.py
+++ ska_helpers/retry/__init__.py
@@ -1,3 +1,24 @@
+"""
+Retry package initially copied from https://github.com/invl/retry.
+
+This project appears to be abandoned so moving it to ska_helpers.
+
+LICENSE:
+
+Copyright 2014 invl
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 __all__ = ['retry']
 
 import logging
