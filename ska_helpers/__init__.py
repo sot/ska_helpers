96e5a53a3505a67cb5977a6f1060374351c8d746
Tom Aldcroft <taldcroft@gmail.com>
2020-12-12 14:02:55 -0500

    Add standard test() function in __init__.py

--- ska_helpers/__init__.py
+++ ska_helpers/__init__.py
@@ -5,3 +5,12 @@ Ska_helpers is a collection of utilities for the Ska3 runtime environment.
 from .version import get_version
 
 __version__ = get_version(__package__)
+
+
+def test(*args, **kwargs):
+    '''
+    Run py.test unit tests.
+    '''
+    import testr
+    return testr.test(*args, **kwargs)
+
