810a3b55c8c30b84e53caae9f32072a9987ddbf0
Tom Aldcroft <taldcroft@gmail.com>
2020-12-17 06:32:09 -0500

    Add lru_cache_timed decorator

--- ska_helpers/tests/test_utils.py
+++ ska_helpers/tests/test_utils.py
@@ -1,6 +1,7 @@
 import pickle
+import time
 
-from ska_helpers.utils import LazyDict, LazyVal
+from ska_helpers.utils import LazyDict, LazyVal, lru_cache_timed
 
 
 def load_func(a, b, c=None):
@@ -42,3 +43,23 @@ def test_lazy_val_pickle():
     x = LazyVal(load_func, 1, 2, c=3)
     xpp = pickle.loads(pickle.dumps(x))
     assert xpp.val == {'a': 1, 'b': 2, 'c': 3}
+
+
+def test_lru_cache_timed():
+    @lru_cache_timed(maxsize=128, timeout=0.1)
+    def test(a):
+        return a + 1
+
+    assert test.cache_info().currsize == 0
+    test(1)
+    assert test.cache_info().currsize == 1
+    time.sleep(0.11)
+    assert test.cache_info().currsize == 0
+    test(1)
+    assert test.cache_info().currsize == 1
+    time.sleep(0.11)
+    test(1)
+    test(2)
+    assert test.cache_info().currsize == 2
+    time.sleep(0.11)
+    assert test.cache_info().currsize == 0
