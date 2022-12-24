f4f35a1f8d65cae841d4e3b2a7cbbcc29c34cb94
Tom Aldcroft <taldcroft@gmail.com>
2022-12-24 16:56:31 -0500

    Improve get_version debug output

--- ska_helpers/version.py
+++ ska_helpers/version.py
@@ -38,8 +38,19 @@ def get_version(package, distribution=None):
         Version string
 
     """
+    import sys
+
+    debug = "SKA_HELPERS_VERSION_DEBUG" in os.environ
+    if debug:
+        print("*" * 80)
+        print(f"Getting version for package={package} distribution={distribution} ")
+        print(f"  Current directory: {Path.cwd()}")
+        print(f"  {sys.path=}")
+
     # Get module file for package.
     module_file = importlib.util.find_spec(package, distribution).origin
+    if debug:
+        print(f"  {module_file=}")
 
     # From this point guarantee tha a version string is returned.
     try:
@@ -48,15 +59,32 @@ def get_version(package, distribution=None):
             # # that gets imported (we check that next).  For some packages, e.g.
             # cheta or pyyaml, the distribution will be different from the
             # package.
+            if debug:
+                print(
+                    "  Getting distribution "
+                    f"dist_info=get_distribution({distribution or package})"
+                )
             dist_info = get_distribution(distribution or package)
             version = dist_info.version
+            if debug:
+                print(f"    {dist_info.location=}")
+                print(f"    {dist_info.version=}")
 
             # Check if the imported package __init__.py file has the same location
             # as the distribution that was found.  If working in a local git repo
             # that does not have a <package>.egg-info directory, get_distribution()
             # will find an installed version.  Windows does not necessarily
             # respect the case so downcase everything.
-            assert module_file.lower().startswith(dist_info.location.lower())
+            ok = module_file.lower().startswith(dist_info.location.lower())
+            if debug:
+                if ok:
+                    print("    distinfo.location matches module_file")
+                else:
+                    print(
+                        "    FAIL: distinfo.location does not match module_file, "
+                        "falling through to setuptools_scm"
+                    )
+            assert ok
 
             # If the dist_info.location appears to be a git repo, then
             # get_distribution() has gotten a "local" distribution and the
@@ -64,31 +92,35 @@ def get_version(package, distribution=None):
             # last run of "setup.py sdist" or "setup.py bdist_wheel", i.e.
             # unrelated to current version, so ignore in this case.
             git_dir = Path(dist_info.location, ".git")
-            if git_dir.exists() and git_dir.is_dir():
-                raise AssertionError
-            if "SKA_HELPERS_VERSION_DEBUG" in os.environ:
-                print(
-                    "** Getting version via DIST_INFO: "
-                    f"package={package} distribution={distribution} "
-                    f"dist_info.location={dist_info.location}"
-                )
+            bad = git_dir.exists() and git_dir.is_dir()
+            if debug:
+                if bad:
+                    print(
+                        "    FAIL: distinfo.location is git repo (version likely wrong), "
+                        "falling through to setuptools_scm"
+                    )
+                else:
+                    print("    distinfo.location looks OK (not a git repo)")
+            assert not bad
 
         except (DistributionNotFound, AssertionError):
             # Get_distribution failed or found a different package from this
             # file, try getting version from source repo.
             from setuptools_scm import get_version
 
+            if debug:
+                print("  Getting version via setuptools_scm for git repo")
+
             # Define root as N directories up from location of __init__.py based
             # on package name.
             roots = [".."] * len(package.split("."))
             if os.path.basename(module_file) != "__init__.py":
                 roots = roots[:-1]
-            if "SKA_HELPERS_VERSION_DEBUG" in os.environ:
-                print(
-                    "** Getting version via setuptools_scm: "
-                    f"package={package} distribution={distribution} "
-                    f"get_version(root={Path(*roots)}, relative_to={module_file})"
-                )
+            if debug:
+                print(f"    Running get_version(")
+                print(f"        root={Path(*roots)},")
+                print(f"        relative_to={module_file}")
+                print(f"    )")
             version = get_version(root=Path(*roots), relative_to=module_file)
 
     except Exception:
@@ -103,7 +135,19 @@ def get_version(package, distribution=None):
             # https://docs.pytest.org/en/latest/pythonpath.html
             warnings.warn(traceback.format_exc() + "\n\n")
             warnings.warn("Failed to find a package version, setting to 0.0.0")
+
         version = "0.0.0"
+
+        if debug:
+            print(f"FAIL: got {version=}")
+
+    else:
+        # No exception, so we got a valid version string.
+        if debug:
+            print(f"SUCCESS: got {version=}")
+
+    if debug:
+        print("*" * 80)
 
     return version
 
