a728250c58710c2b97ffeb05326ffa64fe5cb083
Tom Aldcroft <taldcroft@gmail.com>
2023-11-21 09:11:23 -0500

    Add twiki_diff module to compare TWiki pages and diff

--- setup.py
+++ setup.py
@@ -6,15 +6,26 @@ try:
 except ImportError:
     cmdclass = {}
 
+entry_points = [
+    "ska_twiki_diff = ska_helpers.scripts.twiki_diff:main",
+]
+
+
 setup(
     name="ska_helpers",
     description="Utilities for ska packages",
     author="Javier Gonzalez",
     author_email="javier.gonzalez@cfa.harvard.edu",
     url="http://cxc.harvard.edu/mta/ASPECT/tool_doc/ska_helpers.html",
-    packages=["ska_helpers", "ska_helpers.retry", "ska_helpers.tests"],
+    packages=[
+        "ska_helpers",
+        "ska_helpers.retry",
+        "ska_helpers.tests",
+        "ska_helpers.scripts",
+    ],
     tests_require=["pytest"],
     use_scm_version=True,
     setup_requires=["setuptools_scm", "setuptools_scm_git_archive"],
     cmdclass=cmdclass,
+    entry_points={"console_scripts": entry_points},
 )
