cfadb06829f35cf4bfbba87097b8dab687015de3
Tom Aldcroft <taldcroft@gmail.com>
2025-04-04 16:22:14 -0400

    Add get_conf_module_vars function and update example usage

--- ska_helpers/docs/__init__.py
+++ ska_helpers/docs/__init__.py
@@ -2,27 +2,39 @@
 
 Example usage in docs/conf.py. This is available in ``skare3/templates/conf.py``::
 
-    import os
+  import ska_helpers.docs
+  
+  # Get variables from the ska_helpers.docs.conf module that are not private, modules, or
+  # classes and inject into this namespace. This provides the base configuration for the
+  # Sphinx documentation.
+  globals().update(
+      ska_helpers.docs.get_conf_module_vars(
+          project="chandra_aca",
+          author="Tom Aldcroft",
+      )
+  )
+  
+  # Add any custom configuration here
 
-    import ska_helpers.docs
-
-    context = {
-        "project": "parse_cm",
-        "author": "Tom Aldcroft",
-        "import_path": os.path.abspath(".."),
-    }
-
-    # Get the base Ska conf.py from ska_helpers.docs.conf, putting `context` into the
-    # conf.py namespace.
-    conf = ska_helpers.docs.get_conf_module(context)
-
-    # Inject all the symbols from the ska_helpers.docs.conf module into this namespace.
-    globals().update({k: v for k, v in vars(conf).items() if not k.startswith("_")})
 """
 import sys
 import importlib.util
+import inspect
 
 def get_conf_module(context):
+    """Return the ska_helpers.docs.conf module.
+
+    Parameters
+    ----------
+    context : dict
+        A dictionary of context variables to inject into the conf module.
+        This is typically the project name and author.
+
+    Returns
+    -------
+    module
+        The ska_helpers.docs.conf module with the context variables injected.
+    """
     name = "ska_helpers.docs.conf"
     spec = importlib.util.find_spec(name)
     conf = importlib.util.module_from_spec(spec)
@@ -33,3 +45,31 @@ def get_conf_module(context):
     sys.modules[name] = conf
     spec.loader.exec_module(conf)
     return conf
+
+
+def get_conf_module_vars(**context):
+    """Return the configuration variables from the ska_helpers.docs.conf module.
+
+    Parameters
+    ----------
+    context : dict
+        A dictionary of context variables to inject into the conf module.
+        This is typically the project name, author, and import path.
+        The import path is used to set the sys.path for the conf module.
+
+    Returns
+    -------
+    dict
+        A dictionary of variables from the ska_helpers.docs.conf module.
+        This includes the project name, author, and import path.
+    """
+    conf = get_conf_module(context)
+
+    # Extract the variables from the conf module that are not private, modules, or
+    # classes and return that as a dict.
+    vars_ = {
+        k: v
+        for k, v in vars(conf).items()
+        if not k.startswith("_") and not inspect.ismodule(v) and not inspect.isclass(v)
+    }
+    return vars_
